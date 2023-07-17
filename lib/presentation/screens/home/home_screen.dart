f4d9d8e3e44be5758497ecd7c62f6bca51eb2b02
JordyV23 <jordyvargas0123@gmail.com>
2023-07-16 21:12:28 -0600

    Tareas realizadas: -Implementacion de home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,47 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    appMenuItems[0];
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        return _CustomListTile(index:index);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.index,
+  });
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final theme = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(appMenuItems[index].icon, color: theme.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: theme.primary),
+      title: Text(appMenuItems[index].title),
+      subtitle: Text(appMenuItems[index].subTitle),
+      onTap: (){
+        //navegar a otra pantalla
+      },
     );
   }
 }
